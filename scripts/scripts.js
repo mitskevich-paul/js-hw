dc2d212099350bd3cf6542cf325d7696d2a21d75
mitskevich-paul <58680035+mitskevich-paul@users.noreply.github.com>
2020-02-05 17:00:33 +0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -205,67 +205,40 @@ fifthShowElement.addEventListener('click', event => {
 
 let sixthElement = document.getElementById('sixth');
 sixth.addEventListener('click', event => { 
-let object1 = {
-money: 100,
-house: 10,
-chiks: 3,};
-let object2 = {
-money: 100,
-house: 10,
-chiks: 3,};
-
-let comparision = (object1, object2) => {
-	let result;
-	let obj1array = []; 
-	let obj2array = [];
-  obj1array = Object.keys(object1); 
-  obj2array = Object.keys(object2);
-  let val1array = []; 
-	let val2array = [];
-  val1array = Object.values(object1); 
-  val2array = Object.values(object2);
-  	if (obj1array.length === obj2array.length || val1array.length === val2array.length) {
-      	let leng = 0;
-        while (leng < obj1array.length) {
-        	while (leng < obj2array.length) {
-          	let val1 = obj1array[leng];
-		let val2 = obj2array[leng];
-            leng++;
-            	if (val1 === val2) {
-              		let leng1 = 0;
-      	          while (leng1 < val1array.length) {
-      	            while (leng1 < val2array.length) {
-      	              let val11 = val1array[leng1];
-      	              let val12 = val2array[leng1];
-      	              leng1++;
-      	                if (val11 === val12) {
-                        result = 1;
-      	                } else {
-      	                result = 0;
-      	                }
-      	            } 
-      	         } 
-      	     }  
-        	}  
-			} 
-  }
-  if (result === 1) {
-  return result;
-  } else if (result === 0) {
-  return;
-  }
-}  
-console.log(comparision(object1, object2));
-if (comparision(object1, object2) === 1) {
-alert('Объекты одинаковы!')
-} else {
-alert('Объекты отличаются!')
+const isObject = (obj) => {
+	return obj && typeof obj === 'object';
 }
+
+function deepEquals(a, b) {
+	if (a === b) {
+  	return true;
+  } else if (isObject(a) && isObject(b) && Object.keys(a).length === Object.keys(b).length) {
+  	for (const key in a) {
+    	if (!deepEquals(a[key], b[key])) {
+      	return false;
+      }
+    }
+   	return true;
+  }
+  return false;
+}
+let a = {
+a: 1,
+b: 2,
+c: {a: 1}
+}
+let b = {
+a: 1,
+b: 2,
+c: {a: 1}
+}
+
+console.log(deepEquals(a, b))
 });
 
 let sixthShowElement = document.getElementById('sixth_show');
 sixthShowElement.addEventListener('click', event => {
-    window.open("https://jsfiddle.net/AlfaOne/532pdLhm/5/");
+    window.open("https://jsfiddle.net/AlfaOne/caftzpd9/2/");
 });
 
 let seventhElement = document.getElementById('seventh');
