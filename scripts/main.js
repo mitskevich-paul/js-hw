d8003a1be73580cb36e0de1e303eacafc27ab4a5
mitskevich-paul <58680035+mitskevich-paul@users.noreply.github.com>
2020-02-16 14:47:31 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,88 @@
+let showButton = document.getElementById('showAll');
+showButton.addEventListener('click', () => {
+    document.getElementById('mainMenu').style.animation = "chainsReverse 1.5s cubic-bezier(.63, .60, .48, 1) forwards";
+    document.getElementById('menuSecond').style.animation = "chains 1.5s cubic-bezier(.63, .60, .48, 1) 0.2s forwards";
+});
+let menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', () => {
+    document.getElementById('menuSecond').style.animation = "chainsReverse 1.5s cubic-bezier(.63, .60, .48, 1) forwards";
+    document.getElementById('mainMenu').style.animation = "chains 1.5s cubic-bezier(.63, .60, .48, 1) 0.2s forwards";
+});
+let nextButton = document.getElementById('nextPage');
+nextButton.addEventListener('click', () => {
+    document.getElementById('menuSecond').style.animation = "chainsReverse 1.5s cubic-bezier(.63, .60, .48, 1) forwards";
+    document.getElementById('menuThird').style.animation = "chains 1.5s cubic-bezier(.63, .60, .48, 1) 0.2s  forwards";
+});
+let prevButton = document.getElementById('prevPage');
+prevButton.addEventListener('click', () => {
+    document.getElementById('menuThird').style.animation = "chainsReverse 1.5s cubic-bezier(.63, .60, .48, 1) forwards";
+    document.getElementById('menuSecond').style.animation = "chains 1.5s cubic-bezier(.63, .60, .48, 1) 0.2s forwards";
+});
+let menuLastButton = document.getElementById('lastMenu');
+menuLastButton.addEventListener('click', () => {
+    document.getElementById('menuThird').style.animation = "chainsReverse 1.5s cubic-bezier(.63, .60, .48, 1) forwards";
+    document.getElementById('mainMenu').style.animation = "chains 1.5s cubic-bezier(.63, .60, .48, 1) 0.2s forwards";
+});
+let firstShowElement = document.getElementById('first_show');
+firstShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/fx8gkw35/5/");
+});
+
+let secondShowElement = document.getElementById('second_show');
+secondShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/qzn0sd3o/6/");
+});
+
+let thirdShowElement = document.getElementById('third_show');
+thirdShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/t1qz8aLs/23/");
+});
+
+let fourthShowElement = document.getElementById('fourth_show');
+fourthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/67zps0vg/3/");
+});
+
+let fifthShowElement = document.getElementById('fifth_show');
+fifthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/dar95jeh/1/");
+});
+
+let sixthShowElement = document.getElementById('sixth_show');
+sixthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/caftzpd9/2/");
+});
+
+let seventhElement = document.getElementById('seventh');
+seventhElement.addEventListener('click', () => {
+    window.open("https://mitskevich-paul.github.io/calc/");
+});
+let seventhShowElement = document.getElementById('seventhShow');
+seventhShowElement.addEventListener('click', () => {
+    window.open("https://mitskevich-paul.github.io/calc/");
+});
+
+let eigthShowElement = document.getElementById('eigth');
+eigthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/4uyam9n1/5/");
+});
+
+let ninghtShowElement = document.getElementById('ninght');
+ninghtShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/pyrqc58j/11/");
+});
+
+let tenthShowElement = document.getElementById('tenth');
+tenthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/o7c3Lkdn/5/");
+});
+
+let eleventhShowElement = document.getElementById('eleventh');
+eleventhShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/yg5cqmjf/5/");
+});
+
+let twelfthShowElement = document.getElementById('twelfth');
+twelfthShowElement.addEventListener('click', () => {
+    window.open("https://jsfiddle.net/AlfaOne/u8hef5vk/");
+});
